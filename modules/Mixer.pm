f06647d46bc018ac7d36812ff359ac06518e9da8
Raf <rmouneyres@gmail.com>
2014-01-06 23:43:27 +0100

    ready to move from ->val to tied hash

--- modules/Mixer.pm
+++ modules/Mixer.pm
@@ -5,30 +5,46 @@ package Mixer;
 use strict;
 use warnings;
 
-use Config::IniFiles;
+#use Config::IniFiles;
+use EcaEcs;
 
-my $debug = 0;
-#-----------------------PROJECT INI---------------------------------
-#project file
-my $ini_project = new Config::IniFiles -file => "project.ini";
-die "reading project ini file failed\n" until $ini_project;
-#folder where to store generated files
-my $files_folder = $ini_project->val('project','filesfolder');
-#--------------------------------------------------------------------
-
-sub new {
+sub NewMixer {
 	my $class = shift;
-	open my $file, ">$files_folder/mixer.ecs" or die $!;
-	bless $file, $class;
-	return $file;
+	my $project = shift;
+	my $name = shift;
+	my $ini_name = "mixer_$name";
+	#un objet mixer a:
+	# 1. un fichier de sortie 
+	my $ecasound = EcaEcs->new($project,$ini_name);
+	$ecasound->build_header($project,$ini_name,'nosync');
+	# 2. des paramètres
+	my %mixer = ( 
+		"ecasound" , $ecasound,
+		"name" , $project->val($ini_name,'name'),
+		"port" , $project->val($ini_name,'port'),
+		"config_folder" , $project->val($ini_name,'configfolder'),
+		"midi" , $project->val($ini_name,'generatekm') );
+	bless \%mixer, $class;
+	return \%mixer;
 }
 
-sub build_header {
 
+sub add_track {
+	#my $create_midi_CC = $ini_project->val('ecasound','generatekm'); #enable/disable midiCC control with -km switch
+	
 }
 
-sub from_inifile {
-
+sub get_name {
+	my $self = shift;
+	return $self->{"name"};
+}
+sub get_port {
+	my $self = shift;
+	return $self->{"port"};
+}
+sub is_midi_controllable {
+	my $self = shift;
+	return $self->{"midi"};
 }
 
 1;
--- modules/Plumbing.pm
+++ modules/Plumbing.pm
@@ -10,27 +10,34 @@ use Config::IniFiles;
 my $debug = 0;
 #-----------------------PROJECT INI---------------------------------
 #project file
-my $ini_project = new Config::IniFiles -file => "project.ini";
-die "reading project ini file failed\n" until $ini_project;
-#folder where to store generated files
-my $files_folder = $ini_project->val('project','filesfolder');
-#do we generate plumbing file ?
-my $do_plumbing = $ini_project->val('jack.plumbing','enable');
+# my $ini_project = new Config::IniFiles -file => "project.ini";
+# die "reading project ini file failed\n" until $ini_project;
+# #folder where to store generated files
+# #do we generate plumbing file ?
+# my $do_plumbing = $ini_project->val('jack.plumbing','enable');
 #--------------------------------------------------------------------
 
 sub new {
-      my $class = shift;
-      open my $file, ">$files_folder/jack.plumbing" or die $!;
-      bless $file, $class;
-      return $file;
+	my $class = shift;
+	my $ini_project = shift;
+	if ( $ini_project->val('jack.plumbing','enable') == 1 ) {
+		my $files_folder = $ini_project->val('project','filesfolder');
+		open my $file, ">$files_folder/jack.plumbing" or die $!;
+		bless $file, $class;
+		return $file;
+  	} else {
+  		warn "Plumbing not activated!!\n";
+  		return undef;
+  	}
 }
   
 sub Add {
-	if ($do_plumbing){ 
-		my $file = shift;
+	if (my $file = shift) {
 		my $message = shift;
 		print $file $message . "\n";
-	}	
+	} else {
+		warn "Plumbing not activated!!\n";
+	}
 }
 sub Close {
 	close shift;
