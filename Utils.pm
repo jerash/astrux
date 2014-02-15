e4687d04e5ef62237a7455ee0da5f64cd557abb4
Raf <rmouneyres@gmail.com>
2014-02-15 19:03:31 +0100

    add Utils.pm containing generic functions

---
+++ Utils.pm
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+package Utils;
+
+use strict;
+use warnings;
+
+#replace any non alphanumeric character with % followed by 2char ascii code (nonmixer mimic)
+sub encode_my_ascii {
+	my $characters = shift;
+	$characters =~ s/([^\w!-])/sprintf("%%%X",ord($1))/eg;
+	return $characters;
+}
+#replace any % followed by 2char ascii code by its ascii character (nonmixer mimic)
+sub decode_my_ascii {
+	my $characters = shift;
+	$characters =~ s/%([0-9A-F][0-9A-F])/chr(hex($1))/eg;
+	return $characters;
+}
+
+1;
